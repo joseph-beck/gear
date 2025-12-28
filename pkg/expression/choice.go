1f6337dc72dacc18617182344ebb78004124d7f7
Joseph Beck <joebeck03@icloud.com>
2025-12-28 15:23:38 +0000

    feat: add evaluate for choice expression

--- pkg/expression/choice.go
+++ pkg/expression/choice.go
@@ -1,5 +1,10 @@
 package expression
 
+import (
+	"github.com/joseph-beck/gear/pkg/cst"
+	"github.com/joseph-beck/gear/pkg/err"
+)
+
 type Choice struct {
 	value []Expression
 }
@@ -8,6 +13,30 @@ func (c Choice) Type() ExpressionType {
 	return ChoiceExpression
 }
 
-func (c Choice) Evaluate() (string, error) {
-	return "", nil
+func (c Choice) Evaluate(input string) (Result, error) {
+	if len(input) == 0 {
+		return Result{
+			remaining: input,
+		}, err.EndOfInput
+	}
+
+	for _, expr := range c.value {
+		r, err := expr.Evaluate(input)
+
+		if err != nil {
+			continue
+		}
+
+		tree := cst.New("choice")
+		tree.Add(r.cst)
+
+		return Result{
+			remaining: r.remaining,
+			cst:       tree,
+		}, nil
+	}
+
+	return Result{
+		remaining: input,
+	}, err.FailedToMatch
 }
--- pkg/expression/choice_test.go
+++ pkg/expression/choice_test.go
@@ -3,6 +3,8 @@ package expression
 import (
 	"testing"
 
+	"github.com/joseph-beck/gear/pkg/cst"
+	"github.com/joseph-beck/gear/pkg/err"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,148 @@ func TestChoiceType(t *testing.T) {
 	expr := Choice{}
 
 	assert.Equal(t, ChoiceExpression, expr.Type())
+}
+
+func TestChoiceEvaluate(t *testing.T) {
+	tests := map[string]struct {
+		input          string
+		expr           Choice
+		expectedResult Result
+		expectedError  error
+	}{
+		"match a or b with input a": {
+			input: "a",
+			expr: Choice{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+				cst: cst.CST{
+					Value: "choice",
+					Children: []cst.CST{
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "a",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		"match a or b with input b": {
+			input: "b",
+			expr: Choice{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+				cst: cst.CST{
+					Value: "choice",
+					Children: []cst.CST{
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "b",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		"fail match a or b with input c": {
+			input: "c",
+			expr: Choice{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "c",
+			},
+			expectedError: err.FailedToMatch,
+		},
+		"fail match a or b with empty input": {
+			input: "",
+			expr: Choice{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+			},
+			expectedError: err.EndOfInput,
+		},
+		"match a or b with input ab": {
+			input: "ab",
+			expr: Choice{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "b",
+				cst: cst.CST{
+					Value: "choice",
+					Children: []cst.CST{
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "a",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output, err := test.expr.Evaluate(test.input)
+
+			assert.Equal(t, test.expectedResult, output)
+
+			assert.Equal(t, test.expectedError, err)
+		})
+	}
 }
