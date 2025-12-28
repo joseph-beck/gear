55250a8dfc9ab139ab6b76c213bd47785516fc7b
Joseph Beck <joebeck03@icloud.com>
2025-12-28 15:34:52 +0000

    feat: add evaluate fo sequence expression

--- pkg/expression/sequence.go
+++ pkg/expression/sequence.go
@@ -1,5 +1,10 @@
 package expression
 
+import (
+	"github.com/joseph-beck/gear/pkg/cst"
+	"github.com/joseph-beck/gear/pkg/err"
+)
+
 type Sequence struct {
 	value []Expression
 }
@@ -8,6 +13,29 @@ func (s Sequence) Type() ExpressionType {
 	return SequenceExpression
 }
 
-func (s Sequence) Evaluate() (string, error) {
-	return "", nil
+func (s Sequence) Evaluate(input string) (Result, error) {
+	if len(input) == 0 {
+		return Result{
+			remaining: input,
+		}, err.EndOfInput
+	}
+
+	tree := cst.New("sequence")
+	for _, expr := range s.value {
+		r, err := expr.Evaluate(input)
+
+		if err != nil {
+			return Result{
+				remaining: input,
+			}, err
+		}
+
+		input = r.remaining
+		tree.Add(r.cst)
+	}
+
+	return Result{
+		remaining: input,
+		cst:       tree,
+	}, nil
 }
--- pkg/expression/sequence_test.go
+++ pkg/expression/sequence_test.go
@@ -3,6 +3,8 @@ package expression
 import (
 	"testing"
 
+	"github.com/joseph-beck/gear/pkg/cst"
+	"github.com/joseph-beck/gear/pkg/err"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,151 @@ func TestSequenceType(t *testing.T) {
 	expr := Sequence{}
 
 	assert.Equal(t, SequenceExpression, expr.Type())
+}
+
+func TestSequenceEvaluate(t *testing.T) {
+	tests := map[string]struct {
+		input          string
+		expr           Sequence
+		expectedResult Result
+		expectedError  error
+	}{
+		"match ab with input ab": {
+			input: "ab",
+			expr: Sequence{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+				cst: cst.CST{
+					Value: "sequence",
+					Children: []cst.CST{
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "a",
+								},
+							},
+						},
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "b",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		"match ab with input abc": {
+			input: "abc",
+			expr: Sequence{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "c",
+				cst: cst.CST{
+					Value: "sequence",
+					Children: []cst.CST{
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "a",
+								},
+							},
+						},
+						{
+							Value: "char",
+							Children: []cst.CST{
+								{
+									Value: "b",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		"fail match ab with input a": {
+			input: "a",
+			expr: Sequence{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+			},
+			expectedError: err.EndOfInput,
+		},
+		"fail match ab with input b": {
+			input: "b",
+			expr: Sequence{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "b",
+			},
+			expectedError: err.FailedToMatch,
+		},
+		"fail match ab with empty input": {
+			input: "",
+			expr: Sequence{
+				value: []Expression{
+					Char{
+						value: 'a',
+					},
+					Char{
+						value: 'b',
+					},
+				},
+			},
+			expectedResult: Result{
+				remaining: "",
+			},
+			expectedError: err.EndOfInput,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output, err := test.expr.Evaluate(test.input)
+
+			assert.Equal(t, test.expectedResult, output)
+
+			assert.Equal(t, test.expectedError, err)
+		})
+	}
 }
