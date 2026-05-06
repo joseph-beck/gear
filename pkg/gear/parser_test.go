f28781b405ad34324b4c69d7b55f01bfccc98627
Joseph Beck <joebeck03@icloud.com>
2026-05-06 16:47:46 +0100

    chore: uncomment bench

--- pkg/gear/parser_test.go
+++ pkg/gear/parser_test.go
@@ -259,243 +259,243 @@ func BenchmarkParser_ParseDirectLeftRecursion(b *testing.B) {
 	}
 }
 
-// func BenchmarkParser_ParseIndirectLeftRecursion(b *testing.B) {
-// 	b.ReportAllocs()
+func BenchmarkParser_ParseIndirectLeftRecursion(b *testing.B) {
+	b.ReportAllocs()
 
-// 	for b.Loop() {
-// 		g := NewGrammar()
+	for b.Loop() {
+		g := NewGrammar()
 
-// 		number := NewRule("number", &Choice{
-// 			Value: []Expression{
-// 				&Char{
-// 					Value: '1',
-// 				},
-// 				&Char{
-// 					Value: '2',
-// 				},
-// 				&Char{
-// 					Value: '3',
-// 				},
-// 			},
-// 		})
+		number := NewRule("number", &Choice{
+			Value: []Expression{
+				&Char{
+					Value: '1',
+				},
+				&Char{
+					Value: '2',
+				},
+				&Char{
+					Value: '3',
+				},
+			},
+		})
 
-// 		x := NewRule("x", &NamedRule{
-// 			Value: "expr",
-// 		})
+		x := NewRule("x", &NamedRule{
+			Value: "expr",
+		})
 
-// 		expr := NewRule("expr", &Choice{
-// 			Value: []Expression{
-// 				&Sequence{
-// 					Value: []Expression{
-// 						&NamedRule{
-// 							Value: "x",
-// 						},
-// 						&Char{
-// 							Value: '+',
-// 						},
-// 						&NamedRule{
-// 							Value: "number",
-// 						},
-// 					},
-// 				},
-// 				&NamedRule{
-// 					Value: "number",
-// 				},
-// 			},
-// 		})
+		expr := NewRule("expr", &Choice{
+			Value: []Expression{
+				&Sequence{
+					Value: []Expression{
+						&NamedRule{
+							Value: "x",
+						},
+						&Char{
+							Value: '+',
+						},
+						&NamedRule{
+							Value: "number",
+						},
+					},
+				},
+				&NamedRule{
+					Value: "number",
+				},
+			},
+		})
 
-// 		g.Add(number)
-// 		g.Add(x)
-// 		g.Add(expr)
+		g.Add(number)
+		g.Add(x)
+		g.Add(expr)
 
-// 		parser := New(ParserParam{
-// 			Grammar: g,
-// 		})
+		parser := New(ParserParam{
+			Grammar: g,
+		})
 
-// 		_, _ = parser.Parse("1+2+3", "expr")
-// 	}
-// }
+		_, _ = parser.Parse("1+2+3", "expr")
+	}
+}
 
-// func BenchmarkParser_ParseArithmeticExpression(b *testing.B) {
-// 	b.ReportAllocs()
+func BenchmarkParser_ParseArithmeticExpression(b *testing.B) {
+	b.ReportAllocs()
 
-// 	for b.Loop() {
-// 		number := NewRule(
-// 			"number",
-// 			&OneOrMore{
-// 				Value: Digit,
-// 			},
-// 		)
+	for b.Loop() {
+		number := NewRule(
+			"number",
+			&OneOrMore{
+				Value: Digit,
+			},
+		)
 
-// 		factor := NewRule(
-// 			"factor",
-// 			&Choice{
-// 				Value: []Expression{
-// 					&NamedRule{
-// 						Value: "number",
-// 					},
-// 					&Sequence{
-// 						Value: []Expression{
-// 							&Char{
-// 								Value: '(',
-// 							},
-// 							&NamedRule{
-// 								Value: "arithmetic_expression",
-// 							},
-// 							&Char{
-// 								Value: ')',
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		)
+		factor := NewRule(
+			"factor",
+			&Choice{
+				Value: []Expression{
+					&NamedRule{
+						Value: "number",
+					},
+					&Sequence{
+						Value: []Expression{
+							&Char{
+								Value: '(',
+							},
+							&NamedRule{
+								Value: "arithmetic_expression",
+							},
+							&Char{
+								Value: ')',
+							},
+						},
+					},
+				},
+			},
+		)
 
-// 		term := NewRule(
-// 			"term",
-// 			&Sequence{
-// 				Value: []Expression{
-// 					&NamedRule{
-// 						Value: "factor",
-// 					},
-// 					&ZeroOrMore{
-// 						Value: &Sequence{
-// 							Value: []Expression{
-// 								&Char{
-// 									Value: '*',
-// 								},
-// 								&NamedRule{
-// 									Value: "factor",
-// 								},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		)
+		term := NewRule(
+			"term",
+			&Sequence{
+				Value: []Expression{
+					&NamedRule{
+						Value: "factor",
+					},
+					&ZeroOrMore{
+						Value: &Sequence{
+							Value: []Expression{
+								&Char{
+									Value: '*',
+								},
+								&NamedRule{
+									Value: "factor",
+								},
+							},
+						},
+					},
+				},
+			},
+		)
 
-// 		arithmeticExpression := NewRule(
-// 			"arithmetic_expression", &Sequence{
-// 				Value: []Expression{
-// 					&NamedRule{
-// 						Value: "term",
-// 					},
-// 					&ZeroOrMore{
-// 						Value: &Sequence{
-// 							Value: []Expression{
-// 								&Char{
-// 									Value: '+',
-// 								},
-// 								&NamedRule{
-// 									Value: "term",
-// 								},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		)
+		arithmeticExpression := NewRule(
+			"arithmetic_expression", &Sequence{
+				Value: []Expression{
+					&NamedRule{
+						Value: "term",
+					},
+					&ZeroOrMore{
+						Value: &Sequence{
+							Value: []Expression{
+								&Char{
+									Value: '+',
+								},
+								&NamedRule{
+									Value: "term",
+								},
+							},
+						},
+					},
+				},
+			},
+		)
 
-// 		g := NewGrammar()
+		g := NewGrammar()
 
-// 		g.Add(number)
-// 		g.Add(factor)
-// 		g.Add(term)
-// 		g.Add(arithmeticExpression)
+		g.Add(number)
+		g.Add(factor)
+		g.Add(term)
+		g.Add(arithmeticExpression)
 
-// 		parser := New(ParserParam{
-// 			Grammar: g,
-// 		})
+		parser := New(ParserParam{
+			Grammar: g,
+		})
 
-// 		_, _ = parser.Parse("1+(2*3)", "arithmetic_expression")
-// 	}
-// }
+		_, _ = parser.Parse("1+(2*3)", "arithmetic_expression")
+	}
+}
 
-// func BenchmarkNumber(b *testing.B) {
-// 	b.ReportAllocs()
+func BenchmarkNumber(b *testing.B) {
+	b.ReportAllocs()
 
-// 	for b.Loop() {
-// 		number := NewRule(
-// 			"number",
-// 			&OneOrMore{
-// 				Value: Digit,
-// 			},
-// 		)
+	for b.Loop() {
+		number := NewRule(
+			"number",
+			&OneOrMore{
+				Value: Digit,
+			},
+		)
 
-// 		g := NewGrammar()
+		g := NewGrammar()
 
-// 		g.Add(number)
+		g.Add(number)
 
-// 		parser := New(ParserParam{
-// 			Grammar: g,
-// 		})
+		parser := New(ParserParam{
+			Grammar: g,
+		})
 
-// 		_, _ = parser.Parse("100000000", "number")
-// 	}
-// }
+		_, _ = parser.Parse("100000000", "number")
+	}
+}
 
-// func BenchmarkWord(b *testing.B) {
-// 	b.ReportAllocs()
+func BenchmarkWord(b *testing.B) {
+	b.ReportAllocs()
 
-// 	for b.Loop() {
-// 		word := NewRule(
-// 			"word",
-// 			&OneOrMore{
-// 				Value: Letter,
-// 			},
-// 		)
+	for b.Loop() {
+		word := NewRule(
+			"word",
+			&OneOrMore{
+				Value: Letter,
+			},
+		)
 
-// 		g := NewGrammar()
+		g := NewGrammar()
 
-// 		g.Add(word)
+		g.Add(word)
 
-// 		parser := New(ParserParam{
-// 			Grammar: g,
-// 		})
+		parser := New(ParserParam{
+			Grammar: g,
+		})
 
-// 		_, _ = parser.Parse("helloworld", "word")
-// 	}
-// }
+		_, _ = parser.Parse("helloworld", "word")
+	}
+}
 
-// func BenchmarkSentence(b *testing.B) {
-// 	b.ReportAllocs()
+func BenchmarkSentence(b *testing.B) {
+	b.ReportAllocs()
 
-// 	for b.Loop() {
-// 		sentence := NewRule(
-// 			"sentence",
-// 			&OneOrMore{
-// 				Value: &Sequence{
-// 					Value: []Expression{
-// 						Letter,
-// 						&Choice{
-// 							Value: []Expression{
-// 								&Char{
-// 									Value: ' ',
-// 								},
-// 								&Char{
-// 									Value: ',',
-// 								},
-// 								&Char{
-// 									Value: '.',
-// 								},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		)
+	for b.Loop() {
+		sentence := NewRule(
+			"sentence",
+			&OneOrMore{
+				Value: &Sequence{
+					Value: []Expression{
+						Letter,
+						&Choice{
+							Value: []Expression{
+								&Char{
+									Value: ' ',
+								},
+								&Char{
+									Value: ',',
+								},
+								&Char{
+									Value: '.',
+								},
+							},
+						},
+					},
+				},
+			},
+		)
 
-// 		g := NewGrammar()
+		g := NewGrammar()
 
-// 		g.Add(sentence)
+		g.Add(sentence)
 
-// 		parser := New(ParserParam{
-// 			Grammar: g,
-// 		})
+		parser := New(ParserParam{
+			Grammar: g,
+		})
 
-// 		_, _ = parser.Parse(
-// 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at tellus eros. Donec semper tempus facilisis. Mauris elementum non quam in aliquet. Nulla massa ligula, vehicula in aliquam varius, venenatis ut quam. Nam feugiat enim nec euismod pretium. Proin accumsan iaculis dolor id dignissim. Sed mollis sit amet mi a tincidunt. Proin at felis eget nisl cursus venenatis molestie nec augue. Morbi ut mauris nec risus gravida fringilla vehicula vitae orci. Pellentesque accumsan et mauris id ultricies. Donec tincidunt lorem tortor, id imperdiet lorem imperdiet ut. Pellentesque non quam at sem suscipit vestibulum eget non risus. Mauris auctor volutpat leo, vel interdum velit posuere sed. Donec ultricies justo non leo vestibulum, eu vehicula arcu commodo.",
-// 			"sentence",
-// 		)
-// 	}
-// }
+		_, _ = parser.Parse(
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at tellus eros. Donec semper tempus facilisis. Mauris elementum non quam in aliquet. Nulla massa ligula, vehicula in aliquam varius, venenatis ut quam. Nam feugiat enim nec euismod pretium. Proin accumsan iaculis dolor id dignissim. Sed mollis sit amet mi a tincidunt. Proin at felis eget nisl cursus venenatis molestie nec augue. Morbi ut mauris nec risus gravida fringilla vehicula vitae orci. Pellentesque accumsan et mauris id ultricies. Donec tincidunt lorem tortor, id imperdiet lorem imperdiet ut. Pellentesque non quam at sem suscipit vestibulum eget non risus. Mauris auctor volutpat leo, vel interdum velit posuere sed. Donec ultricies justo non leo vestibulum, eu vehicula arcu commodo.",
+			"sentence",
+		)
+	}
+}
